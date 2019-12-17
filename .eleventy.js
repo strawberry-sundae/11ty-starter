6eba9fb11f204a4493bfa096c4820a2749fb1fa7
Ryan P <ryanplummer@Ryans-MacBook-Air.local>
2019-12-17 14:06:47 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
     passthroughFileCopy: true,
-    templateFormats: ['njk', 'md', 'css', 'html', 'yml', 'liquid'],
+    templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
     htmlTemplateEngine: 'njk'
   }
 }
